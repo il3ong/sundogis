e0beb94d9b574c4676e9858c7b4f5286a3b6a99f
il3ong <bbonga0609@gmail.com>
2023-10-05 10:33:04 +0900

    offcanvas 대공사 하기 전

--- main.js
+++ main.js
@@ -21,36 +21,21 @@
 //     });
 // });
 
-// $(function () {
-//     $('#datepicker').datepicker({
-//         calendarWeeks: false,
-//         todayHighlight: true,
-//         autoclose: true,
-//         format: "yyyy-mm-dd",
-//         language: "kr"
-//     });
-// });
+$(function () {
+    $('#datepicker').datepicker({
+        calendarWeeks: false,
+        todayHighlight: true,
+        autoclose: true,
+        format: "yyyy-mm-dd",
+        language: "kr"
+    });
+});
 
-
-// Full calendar 
-document.addEventListener('DOMContentLoaded', function() {
-	var calendarEl = document.getElementById('calendar');
-	var calendar = new FullCalendar.Calendar(calendarEl, {
-		initialView : 'dayGridMonth', // 초기 로드 될때 보이는 캘린더 화면(기본 설정: 달)
-		headerToolbar : { // 헤더에 표시할 툴 바
-			start : 'prev next today',
-			center : 'title',
-			end : 'dayGridMonth,dayGridWeek,dayGridDay'
-		},
-		titleFormat : function(date) {
-			return date.date.year + '년 ' + (parseInt(date.date.month) + 1) + '월';
-		},
-		//initialDate: '2021-07-15', // 초기 날짜 설정 (설정하지 않으면 오늘 날짜가 보인다.)
-		selectable : true, // 달력 일자 드래그 설정가능
-		droppable : true,
-		editable : true,
-		nowIndicator: true, // 현재 시간 마크
-		locale: 'ko' // 한국어 설정
-	});
-	calendar.render();
+// datePicker 날짜 선택 이벤트
+$('.calendar-date').datepicker().on('changeDate', function (e) {
+    // 선택한 날짜에서 연도와 월을 가져옵니다.
+    const selectedDate = e.date;
+    const year = selectedDate.getFullYear();
+    const month = selectedDate.getMonth() + 1; // 월은 0부터 시작하므로 +1 해줍니다.
+    const day = selectedDate.getDate();
 });
--- prac.js
+++ prac.js
@@ -2,10 +2,14 @@ const baseMapBtn = document.getElementById("base-map-btn");
 const satelliteMapBtn = document.getElementById("satellite-map-btn");
 const hybridMapBtn = document.getElementById("hybrid-map-btn");
 const downloadBtn = document.getElementById("download-btn");
-const menuHideBtn = document.getElementById("menu-hide-btn");
-const mainView = document.getElementById("main-view");
-const bigMap = document.getElementById("big-map");
-const bigView = document.getElementById("big-view");
+const menuShowBtn = document.getElementById("menu-show-btn");
+
+const giheung = document.querySelector('.giheung');
+const cheoin = document.querySelector('.cheoin');
+const suji = document.querySelector('.suji');
+
+
+
 
 const apiKey = '01FC9396-78C3-3A58-99A4-EF97461DFFEE';
 let map;
@@ -32,24 +36,6 @@ window.addEventListener("load", function () {
 
     });
 
-    // var mapTwo = new ol.Map({
-    //     target: 'big-map',
-    //     layers: [
-    //         new ol.layer.Tile({
-    //             source: new ol.source.XYZ({
-    //                 url: `https://api.vworld.kr/req/wmts/1.0.0/${apiKey}/Base/{z}/{y}/{x}.png`
-    //             })
-    //         })
-    //     ],
-    //     view: new ol.View({
-    //         center: ol.proj.transform([127.1775537, 37.2410864], 'EPSG:4326', 'EPSG:3857'),
-    //         zoom: 11,
-    //         minZoom: 0,
-    //         maxZoom: 21
-    //     })
-
-    // });
-
     //레이어 추가
     // var boundary = new ol.layer.Tile({
     //     source: new ol.source.TileWMS({
@@ -73,6 +59,40 @@ window.addEventListener("load", function () {
 
 
 });
+
+// offcanvas 사용시 화면 비율 조정
+// document.addEventListener('DOMContentLoaded', function () {
+
+//     let viewMap = document.getElementById('map');
+//     menuShowBtn.onclick = function () {
+//         viewMap.style.width = '70%';
+//         viewMap.style.position = 'absolute';
+//         viewMap.style.right = '0';
+//         menuShowBtn.style.display = "none";
+//     }
+
+//     let btnClose = document.querySelector('.btn-close');
+
+//     btnClose.onclick = function () {
+//         viewMap.style.width = '100%';
+//         menuShowBtn.style.display = "";
+//     }
+// });
+
+var myOffcanvas = document.getElementById('myOffcanvas')
+myOffcanvas.addEventListener('hidden.bs.offcanvas', function () {
+    let viewMap = document.getElementById('map');
+    viewMap.style.width = '70%';
+    viewMap.style.position = 'absolute';
+    viewMap.style.right = '0';
+    menuShowBtn.style.display = "none";
+})
+myOffcanvas.addEventListener('show.bs.offcanvs', function () {
+    let viewMap = document.getElementById('map');
+    viewMap.style.width = '100%';
+    menuShowBtn.style.display = "";
+})
+
 
 // 일반 지도
 const baseMap = new ol.layer.Tile({
@@ -102,16 +122,21 @@ const hybridMap = new ol.layer.Tile({
     })
 });
 
+
+
 // 각 버튼 클릭 이벤트 리스너 추가
+// 맵 유형 클릭
 baseMapBtn.addEventListener('click', baseMapClick);
 satelliteMapBtn.addEventListener('click', satelliteMapClick);
 hybridMapBtn.addEventListener('click', hybridMapClick);
 
+// 구 클릭
+giheung.addEventListener('click', giheungClick);
+cheoin.addEventListener('click', cheoinClick);
+suji.addEventListener('click', sujiClick);
+
 // 다운로드 버튼 클릭시
 downloadBtn.addEventListener('click', downloadClick);
-
-// 메뉴 숨기기 버튼 클릭시
-// menuHideBtn.addEventListener('click',menuHideBtnClick);
 
 // 지도 유형 클릭
 function mapTypeClick(layer) {
@@ -137,6 +162,50 @@ function satelliteMapClick() {
 // 하이브리드 지도 클릭 이벤트 핸들러
 function hybridMapClick() {
     mapTypeClick(hybridMap);
+}
+
+// 지역(구) 클릭 이벤트 핸들러
+// 처인구 클릭
+function cheoinClick() {
+
+    map.getView().setCenter(ol.proj.transform([127.201374, 37.234295], 'EPSG:4326', 'EPSG:3857'));
+
+    cheoin.style.backgroundColor = '#0D6EFD';
+    cheoin.style.color = 'white';
+
+    giheung.style.background = 'white';
+    giheung.style.color = 'black';
+    suji.style.background = 'white';
+    suji.style.color = 'black';
+}
+
+// 기흥구 클릭
+function giheungClick() {
+
+
+    map.getView().setCenter(ol.proj.transform([127.114716, 37.280386], 'EPSG:4326', 'EPSG:3857'));
+    giheung.style.backgroundColor = '#0D6EFD';
+    giheung.style.color = 'white';
+
+    cheoin.style.background = 'white';
+    cheoin.style.color = 'black';
+    suji.style.background = 'white';
+    suji.style.color = 'black';
+}
+
+
+// 수지구 클릭
+function sujiClick() {
+
+    map.getView().setCenter(ol.proj.transform([127.097692, 37.322097], 'EPSG:4326', 'EPSG:3857'));
+
+    suji.style.backgroundColor = '#0D6EFD';
+    suji.style.color = 'white';
+
+    cheoin.style.background = 'white';
+    cheoin.style.color = 'black';
+    giheung.style.background = 'white';
+    giheung.style.color = 'black';
 }
 
 // csv 다운로드 클릭 함수
@@ -177,11 +246,11 @@ function getCSV(filename) {
 function downloadCSV(csv, filename) {
     var csvFile;
     var downloadLink;
-  
+
     //한글 처리를 해주기 위해 BOM 추가하기
     const BOM = "\uFEFF";
     csv = BOM + csv;
-  
+
     csvFile = new Blob([csv], { type: "text/csv" });
     downloadLink = document.createElement("a");
     downloadLink.download = filename;
@@ -189,58 +258,4 @@ function downloadCSV(csv, filename) {
     downloadLink.style.display = "none";
     document.body.appendChild(downloadLink);
     downloadLink.click();
-  }
-
-// 상세메뉴 숨김
-// function menuHideBtnClick(){
-//     mainView.style.display="none";
-//     bigView.style.display="";
-// }
-
-
-// //  center과 경계 설정
-// //  좌표 설정 필수
-// var view = new ol.View({
-//    center : ol.proj.transform([126.78566, 37.50526], 'EPSG:4326', 'EPSG:3857'),
-//    zoom : 12,
-//    extent : ol.proj.transformExtent([ 116.0, 30.0, 136.0, 45.0 ], 'EPSG:4326', 'EPSG:3857'),
-//    maxZoom : 18,
-//    minZoom : 6
-// });
-
-// //  지도 그리기
-// map = new ol.Map({
-//    pixelRatio : 1,
-//    controls : ol.control.defaults({
-//       attribution : false,      //지도 컨트롤하는 부분
-//       zoom : false
-//    }),
-//    layers : [ sido_layer ],
-//    interactions : ol.interaction.defaults({
-//       shiftDragZoom : true
-//    }),
-//    target : document.getElementById('map'),   //map영역을 target으로 잡겠다
-//    view : view,
-//    logo : false
-// });
-
-
-// // 기존 Zoom 컨트롤 제거
-// map.getControls().forEach(function (control) {
-//     if (control instanceof ol.control.Zoom) {
-//         map.removeControl(control);
-//     }
-// });
-
-// // 줌 컨트롤러 생성
-// let zoomControl = new ol.control.Zoom();
-
-// // 오른쪽 상단으로 컨트롤러를 이동
-// let zoomControlDiv = document.createElement('div');
-// zoomControlDiv.className = 'ol-control ol-unselectable zoom-control';
-// zoomControlDiv.appendChild(zoomControl.element);
-// map.addControl(new ol.control.Control({
-//     element: zoomControlDiv,
-//     target: 'map'
-
-// }));
+}
