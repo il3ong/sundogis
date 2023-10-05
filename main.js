3072441e3f51a99ed9dbf659715ea5e075344a04
il3ong <bbonga0609@gmail.com>
2023-10-05 20:30:24 +0900

    각 구역 구현, 다운로드, 업로드, 맵 띄우기 남음

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // $(document).ready(function () {
-//     $("#uploadButton").click(function () {
+//     $("#csv-register-btn").click(function () {
 //         var formData = new FormData($("#uploadForm")[0]);
 
 //         $.ajax({
@@ -21,6 +21,8 @@
 //     });
 // });
 
+
+//  datePicker 설정
 $(function () {
     $('#datepicker').datepicker({
         calendarWeeks: false,
@@ -38,4 +40,103 @@ $('.calendar-date').datepicker().on('changeDate', function (e) {
     const year = selectedDate.getFullYear();
     const month = selectedDate.getMonth() + 1; // 월은 0부터 시작하므로 +1 해줍니다.
     const day = selectedDate.getDate();
+});
+
+
+// < DATA 모달창 !!!!!!!!!!!!!!!!!!!!!!!>
+// 모달창 show
+$('#add-data-btn').click(function (e) {
+    e.preventDefault();
+    $('#add-data-modal').modal("show");
+});
+
+// 모달창 hide
+$('.csv-cancel-btn').click(function (e) {
+    e.preventDefault();
+    $('#add-data-modal').modal("hide");
+    
+    var gpsFile = $('#gpsfile');
+    var noiseFile = $('#noisefile');
+    var rpmFile = $('#rpmfile');
+    
+    gpsFile.val('');
+    noiseFile.val('');
+    rpmFile.val('');
+  
+});
+
+// gps-csv
+$(document).ready(function () {
+    $("#gps-csv").on('change', function () {  // 값이 변경되면
+        if (window.FileReader) {  // modern browser
+            var filename = $(this)[0].files[0].name;
+        } else {  // old IE
+            var filename = $(this).val().split('/').pop().split('\\').pop();  // 파일명만 추출
+        }
+
+        // 추출한 파일명 삽입
+        $("#gpsfile").val(filename);
+    });
+});
+
+// noise-csv
+$(document).ready(function () {
+    $("#noise-csv").on('change', function () {  // 값이 변경되면
+        if (window.FileReader) {  // modern browser
+            var filename = $(this)[0].files[0].name;
+        } else {  // old IE
+            var filename = $(this).val().split('/').pop().split('\\').pop();  // 파일명만 추출
+        }
+
+        // 추출한 파일명 삽입
+        $("#noisefile").val(filename);
+    });
+});
+
+// rpm-csv
+$(document).ready(function () {
+    $("#rpm-csv").on('change', function () {  // 값이 변경되면
+        if (window.FileReader) {  // modern browser
+            var filename = $(this)[0].files[0].name;
+        } else {  // old IE
+            var filename = $(this).val().split('/').pop().split('\\').pop();  // 파일명만 추출
+        }
+
+        // 추출한 파일명 삽입
+        $("#rpmfile").val(filename);
+    });
+});
+
+// csv 파일만 업로드가능
+const csvFileInputs = document.querySelectorAll('.csv-file-input');
+
+csvFileInputs.forEach((input) => {
+  input.addEventListener('change', (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      const fileName = selectedFile.name;
+      if (fileName.endsWith('.csv')) {
+
+      } else {
+        alert('올바른 .csv 파일을 선택해주세요.');
+        input.value = '';
+      }
+    }
+  });
+});
+
+
+// < DATA 모달창 끝 !!!!!!!!!!!!!!!!!!!!!!!>
+
+// < CAR 모달창 >
+// 모달창 show
+$('#add-car-btn').click(function (e) {
+    e.preventDefault();
+    $('#add-car-modal').modal("show");
+});
+
+// 모달창 hide
+$('.car-cancel-btn').click(function (e) {
+    e.preventDefault();
+    $('#add-car-modal').modal("hide");
 });
--- prac copy.js
+++ prac copy.js
@@ -14,9 +14,6 @@ const suji = document.querySelector('.suji');
 const apiKey = '01FC9396-78C3-3A58-99A4-EF97461DFFEE';
 
 let map;
-let mapType = "Base";
-let imgType = "png";
-
 
 // 용인시 경계
 const boundary = new ol.layer.Tile({
@@ -72,10 +69,42 @@ const sujiBoundary = new ol.layer.Tile({
 });
 
 
-//  첫 화면 생성
-startView();
+// 일반 지도
+const baseMap = new ol.layer.Tile({
+    name: 'base',
+    visible: true,
+    source: new ol.source.XYZ({
+        url: `https://api.vworld.kr/req/wmts/1.0.0/${apiKey}/Base/{z}/{y}/{x}.png`
+    })
+});
 
+// 위성 지도 
+const satelliteMap = new ol.layer.Tile({
+    name: 'Satellite',
+    visible: true,
+    source: new ol.source.XYZ({
+        url: `http://api.vworld.kr/req/wmts/1.0.0/${apiKey}/Satellite/{z}/{y}/{x}.jpeg`
+    })
+});
+
+// 하이브리드 지도
+const hybridMap = new ol.layer.Tile({
+    name: 'Hybrid',
+    visible: true,
+    source: new ol.source.XYZ({
+        url: `http://api.vworld.kr/req/wmts/1.0.0/${apiKey}/Hybrid/{z}/{y}/{x}.png`
+    })
+});
+
+
+//  첫 화면 생성
+
+// window.addEventListener("load", function () {
+// });
+startView();
 function startView() {
+    let mapType = "Base";
+    let imgType = "png";
     map = new ol.Map({
         target: 'map',
         // 뷰, style 등을 관리하기 위해 설정
@@ -96,18 +125,31 @@ function startView() {
 
     //레이어 추가
     map.addLayer(boundary);
-    boundary.setOpacity(0.3);
+    boundary.setOpacity(0.5);
     // console.log(`Current map type: ${mapType}`);
+
+    //  전체화면
+    var myFullScreenControl = new ol.control.FullScreen();
+    map.addControl(myFullScreenControl);
 }
 
-//  전체화면
-var myFullScreenControl = new ol.control.FullScreen();
-map.addControl(myFullScreenControl);
 
 
-baseMapBtn.addEventListener('click', () => {
-    mapType = "Base";
-    imgType = "png";
+
+// 지도 유형 클릭
+function mapTypeClick(layer) {
+    // 모든 레이어를 지도에서 제거
+    map.getLayers().forEach((mapLayer) => {
+        map.removeLayer(mapLayer);
+    });
+    layer.setZIndex(-1);
+    // 선택한 레이어를 지도에 추가
+    map.addLayer(layer);
+    map.addLayer(boundary);
+}
+
+// 일반 지도 클릭 이벤트 핸들러
+function baseMapClick() {
     baseMapBtn.style.backgroundColor = '#293661';
     baseMapBtn.style.color = 'white';
 
@@ -115,12 +157,11 @@ baseMapBtn.addEventListener('click', () => {
     satelliteMapBtn.style.color = 'black';
     hybridMapBtn.style.background = 'white';
     hybridMapBtn.style.color = 'black';
-    updateBackgroundLayer();
-})
+    mapTypeClick(baseMap);
+}
 
-satelliteMapBtn.addEventListener('click', () => {
-    mapType = "Satellite";
-    imgType = "jpeg";
+// 위성 지도 클릭 이벤트 핸들러
+function satelliteMapClick() {
     satelliteMapBtn.style.backgroundColor = '#293661';
     satelliteMapBtn.style.color = 'white';
 
@@ -128,12 +169,11 @@ satelliteMapBtn.addEventListener('click', () => {
     baseMapBtn.style.color = 'black';
     hybridMapBtn.style.background = 'white';
     hybridMapBtn.style.color = 'black';
-    updateBackgroundLayer();
-})
+    mapTypeClick(satelliteMap);
+}
 
-hybridMapBtn.addEventListener('click', () => {
-    mapType = "Hybrid";
-    imgType = "png";
+// 하이브리드 지도 클릭 이벤트 핸들러
+function hybridMapClick() {
     hybridMapBtn.style.backgroundColor = '#293661';
     hybridMapBtn.style.color = 'white';
 
@@ -141,26 +181,15 @@ hybridMapBtn.addEventListener('click', () => {
     baseMapBtn.style.color = 'black';
     satelliteMapBtn.style.background = 'white';
     satelliteMapBtn.style.color = 'black';
-    updateBackgroundLayer();
-})
-
-function updateBackgroundLayer() {
-    const newBackgroundLayer = new ol.layer.Tile({
-        source: new ol.source.XYZ({
-            url: `https://api.vworld.kr/req/wmts/1.0.0/${apiKey}/${mapType}/{z}/{y}/{x}.${imgType}`
-        })
-    });
-
-    map.getLayers().setAt(0, newBackgroundLayer); // 현재 첫 번째 레이어를 교체
+    mapTypeClick(hybridMap);
 }
 
-
+// 지역(구) 클릭 이벤트 핸들러
 // 처인구 클릭
-cheoin.addEventListener('click', () => {
+function cheoinClick() {
+    map.getView().setCenter(ol.proj.transform([127.201374, 37.234295], 'EPSG:4326', 'EPSG:3857'));
+    map.addLayer(cheoinBoundary);
 
-    // 새로운 레이어 추가
-    const cheoinMap = map.getView().setCenter(ol.proj.transform([127.201374, 37.234295], 'EPSG:4326', 'EPSG:3857'));
-    // mapTypeClick(cheoinMap);
     cheoin.style.backgroundColor = '#293661';
     cheoin.style.color = 'white';
 
@@ -168,14 +197,13 @@ cheoin.addEventListener('click', () => {
     giheung.style.color = 'black';
     suji.style.background = 'white';
     suji.style.color = 'black';
-})
+}
 
 // 기흥구 클릭
-giheung.addEventListener('click', () => {
+function giheungClick() {
+    map.getView().setCenter(ol.proj.transform([127.114716, 37.280386], 'EPSG:4326', 'EPSG:3857'));
+    map.addLayer(giheungBoundary);
 
-    // 새로운 레이어 추가
-    const giheungMap = map.getView().setCenter(ol.proj.transform([127.114716, 37.280386], 'EPSG:4326', 'EPSG:3857'));
-    // mapTypeClick(giheungMap);
     giheung.style.backgroundColor = '#293661';
     giheung.style.color = 'white';
 
@@ -183,15 +211,13 @@ giheung.addEventListener('click', () => {
     cheoin.style.color = 'black';
     suji.style.background = 'white';
     suji.style.color = 'black';
-
-})
+}
 
 // 수지구 클릭
-suji.addEventListener('click', () => {
+function sujiClick() {
+    map.getView().setCenter(ol.proj.transform([127.097692, 37.322097], 'EPSG:4326', 'EPSG:3857'));
+    map.addLayer(sujiBoundary);
 
-    // 새로운 레이어 추가
-    const sujiMap = map.getView().setCenter(ol.proj.transform([127.097692, 37.322097], 'EPSG:4326', 'EPSG:3857'));
-    // mapTypeClick(sujiMap);
     suji.style.backgroundColor = '#293661';
     suji.style.color = 'white';
 
@@ -199,18 +225,8 @@ suji.addEventListener('click', () => {
     cheoin.style.color = 'black';
     giheung.style.background = 'white';
     giheung.style.color = 'black';
-})
-
-// 지도 유형 클릭 초기화 작업중
-function mapTypeClick(layer) {
-    // 모든 레이어를 지도에서 제거
-    map.getLayers().forEach((mapLayer) => {
-        map.removeLayer(mapLayer);
-    });
-    // layer.setZIndex(-1);
-    // 선택한 레이어를 지도에 추가
-    map.addLayer(layer);
 }
+
 // offcanvas 사용시 화면 비율 조정
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -236,6 +252,17 @@ liveBtn.addEventListener('click', () => {
     menuShowBtn.classList.toggle('hide-btn');
     liveBtn.classList.toggle('blinkin-btn');
 });
+
+// 각 버튼 클릭 이벤트 리스너 추가
+// 맵 유형 클릭
+baseMapBtn.addEventListener('click', baseMapClick);
+satelliteMapBtn.addEventListener('click', satelliteMapClick);
+hybridMapBtn.addEventListener('click', hybridMapClick);
+
+// 구 클릭
+giheung.addEventListener('click', giheungClick);
+cheoin.addEventListener('click', cheoinClick);
+suji.addEventListener('click', sujiClick);
 
 // // 다운로드 버튼 클릭시
 // downloadBtn.addEventListener('click', downloadClick);
